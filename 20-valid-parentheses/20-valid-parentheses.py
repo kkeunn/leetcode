b4bf7d56655cc4c5c2b4123b3285bd898634bdc0
kkeunn <96153290+kkeunn@users.noreply.github.com>
2022-07-05 02:14:12 +0900

    Time: 46 ms (11.11%), Space: 13.6 MB (59.77%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        s_dict = {")": "(", "}": "{", "]": "["}
+        for i in s:
+            if i in s_dict.values():
+                stack.append(i)
+            else:
+                if len(stack) != 0 and s_dict[i] == stack[-1]:
+                    stack.pop()
+                else:
+                    return False
+                
+        if len(stack) == 0:
+            return True
+        else:
+            return False
