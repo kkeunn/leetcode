bad2e4ff4a265c152266441e317eab8a1d3e1310
kkeunn <96153290+kkeunn@users.noreply.github.com>
2022-07-07 04:48:59 +0900

    Time: 81 ms (55.16%), Space: 13.7 MB (35.62%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -7,18 +7,21 @@ class Solution(object):
         :type color: int
         :rtype: List[List[int]]
         """
-        m, n = len(image), len(image[0])
-        srsc_color = image[sr][sc]
-        
         def dfs(i, j):
-            # 픽셀의 색을 변경
+            # x와 y가 범위 안에 있지 않거나 (sr, sc)에서의 색과 같지 않다면 종료
+            if i < 0 or i >= len(image) or \
+                j < 0 or j >= len(image[0]) or\
+                image[i][j] != srsc_color:
+                    return
+
             image[i][j] = color
             # 동서남북 탐색
-            for x, y in [(i, j+1), (i, j-1), (i+1, j), (i-1, j)]:
-                # x와 y가 범위 안에 있고 (sr, sc)에서의 색과 같다면
-                if 0 <= x < m and 0 <= y < n and  image[x][y] == srsc_color:
-                    # 재귀 호출
-                    dfs(x, y)
+            dfs(i, j+1)
+            dfs(i, j-1)
+            dfs(i+1, j)
+            dfs(i-1, j)
+            
+        srsc_color = image[sr][sc]
         
         if srsc_color != color:
             dfs(sr, sc)
