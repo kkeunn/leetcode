d19020b3920d6ec11c7125353053d7cb9cbdee37
kkeunn <96153290+kkeunn@users.noreply.github.com>
2022-07-12 07:35:02 +0900

    Time: 217 ms (28.76%), Space: 15.2 MB (33.27%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        strs = []
+        for char in s:
+            if char.isalnum(): # 영문자, 숫자 여부 판별
+                strs.append(char.lower())
+                
+        while len(strs) > 1:
+            if strs.pop(0) != strs.pop():
+                return False
+            
+        return True
