8ae3733a7735aff7fd5d3e87f8d8090782e5a026
kkeunn <96153290+kkeunn@users.noreply.github.com>
2022-07-13 08:04:16 +0900

    Time: 389 ms (58.02%), Space: 16.6 MB (78.40%) - LeetHub

--- 561-array-partition/561-array-partition.py
+++ 561-array-partition/561-array-partition.py
@@ -1,6 +1,5 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
         nums.sort()
-        odd_list = nums[::2]
         
-        return sum(odd_list)
+        return sum(nums[::2])
